45508f25423896bba98a6b56edfa93098128dcc1
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2019-07-17 22:38:50 +0300

    Git editor/windows: fix default path to VS Code

--- git-editor-windows.sh
+++ git-editor-windows.sh
@@ -5,7 +5,7 @@ echo Check README.md for details.
 #
 #git config --global core.editor "'C:/Program Files/emacs/bin/emacsclient.exe'"
 #
-#git config --global core.editor "'~/AppData/Local/Programs/Microsoft VS Code/bin/code.exe' --wait"
+#git config --global core.editor "'~/AppData/Local/Programs/Microsoft VS Code/code.exe' --wait"
 #
 #git config --global core.editor "'C:/Program Files (x86)/sublime text 3/subl.exe' -w"
 #
