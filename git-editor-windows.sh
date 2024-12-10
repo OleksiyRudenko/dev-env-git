1e3ddb048d74d4b0b4377758e08f3ed6682f1c9c
Oleksiy Rudenko <8686631+OleksiyRudenko@users.noreply.github.com>
2024-12-10 14:10:23 +0100

    Update git-editor-windows.sh
    Add instructions for VSCode under WSL

--- git-editor-windows.sh
+++ git-editor-windows.sh
@@ -1,6 +1,7 @@
 echo You need to uncomment and edit a set of instructions corresponding to the editor of your choice.
 echo Check README.md for details.
 #
+# === Windows and git for Windows
 #git config --global core.editor "'C:/Program Files (x86)/Notepad++/notepad++.exe' -multiInst -notabbar -nosession -noPlugin '$*'"
 #
 #git config --global core.editor "'C:/Program Files/emacs/bin/emacsclient.exe'"
@@ -9,6 +10,11 @@ echo Check README.md for details.
 #
 #git config --global core.editor "'C:/Program Files (x86)/sublime text 3/subl.exe' -w"
 #
+# === Linux/MacOS
 #git config --global core.editor "nano"
+#
+# === WSL (Windows Subsystem Linux). Requires Windows version of VS Code installed. Replace USERNAME as appropriate.
+#echo "alias vscode='/mnt/c/Users/USERNAME/AppData/Local/Programs/Microsoft\ VS\ Code/Code.exe'" >> $HOME/.bash_profile
+#git config --global core.editr "vscode --wait"
 #
 git config --global -e
